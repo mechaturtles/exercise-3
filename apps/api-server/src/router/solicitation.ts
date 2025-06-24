f9f1c47dc8952d93b6e5fe2cc0e3d04d5755bb69
Bryce V <bryce.christian@gmail.com>
2025-06-23 21:12:31 -0400

    feat(solicitation): refactor trpc solicitation endpoints and add method types

--- apps/api-server/src/router/solicitation.ts
+++ apps/api-server/src/router/solicitation.ts
@@ -1,15 +1,27 @@
 import db from "@repo/database";
-import { solicitations } from "@repo/database/src/schema/solicitation";
-import { createTRPCRouter, t } from "../trpc";
+import { solicitations } from "@repo/database";
+import { createTRPCRouter, procedure} from "trpc";
 import { z } from "zod";
 import { eq } from "drizzle-orm";
 
+export const SolicitationGetAllOpts = z.object({
+  limit: z.number().optional(),
+  offset: z.number().optional(),
+});
+export type SolicitationGetAllOpts = z.infer<typeof SolicitationGetAllOpts>;
+
+export const SolicitationGetByIdOpts = z.object({
+  id: z.number(),
+});
+export type SolicitationGetByIdOpts = z.infer<typeof SolicitationGetByIdOpts>;
+
+export type SolicitationGetAllRes = Awaited<ReturnType<typeof solicitationRouter.getAll>>;
+export type SolicitationGetByIdRes = Awaited<ReturnType<typeof solicitationRouter.getById>>;
+
+
 export const solicitationRouter = createTRPCRouter({
-  getAll: t.procedure
-    .input(z.object({
-      limit: z.number().optional(),
-      offset: z.number().optional(),
-    }))
+  getAll: procedure
+    .input(SolicitationGetAllOpts)
     .query(async ({ input }) => {
       const limit = input?.limit || 20; // Default to 20 if no limit is provided
       const offset = input?.offset|| 0; // Default to 0 if no start is provided
@@ -20,10 +32,8 @@ export const solicitationRouter = createTRPCRouter({
         .offset(offset);
       return solicitationsPage;
     }),
-  getById: t.procedure
-    .input(z.object({
-      id: z.number(),
-    }))
+  getById: procedure
+    .input(SolicitationGetByIdOpts)
     .query(async ({ input }) => {
       const solicitation = await db
         .select()
--- apps/api-server/src/types.ts
+++ apps/api-server/src/types.ts
@@ -1,3 +1,4 @@
 export type { AppRouter } from "./router";
 
 export type * from "./router/hello-world";
+export type * from "./router/solicitation";
